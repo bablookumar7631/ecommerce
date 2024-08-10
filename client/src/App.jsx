ff89e4e3febeda59a79b9198d203a6a78f3d102d
Babloo Kumar <bablookumar24bk@gmail.com>
2024-08-10 16:49:19 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-red-500 text-4xl flex justify-center mt-5'>Hello world!</h1>
+    </div>
+  )
+}
+
+export default App
+
