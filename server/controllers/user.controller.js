430966bd8294442380b275d6d365b71d1c863493
Babloo Kumar <bablookumar24bk@gmail.com>
2024-08-12 17:50:41 +0530

    add login controller with jwt

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,7 +1,6 @@
-import { ApiError } from "../utils/ApiError.js";
-import { asyncHandler } from "../utils/asyncHandler.js";
 import { User } from "../models/user.model.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 
 
@@ -59,5 +58,76 @@ const register = async(req, res) => {
 }
 
 
+const login = async (req, res) => {
+    try {
+        const { email, password, designation } = req.body;
 
-export {register}
+        if (!email || !password || !designation) {
+            return res.status(400).json({
+                message: "All fields are required",
+                success: false
+            });
+        }
+
+        let user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({
+                message: "Incorrect email or password.",
+                success: false,
+            });
+        }
+
+        const isPasswordMatch = await bcrypt.compare(password, user.password);
+        if (!isPasswordMatch) {
+            return res.status(400).json({
+                message: "Incorrect email or password.",
+                success: false,
+            });
+        }
+
+        if (user.designation !== designation) {
+            return res.status(400).json({
+                message: "Account doesn't exist with the current designation.",
+                success: false
+            });
+        }
+
+        const tokenData = {
+            userId: user._id
+        };
+        const token = jwt.sign(tokenData, process.env.SECRET_KEY, { expiresIn: '1d' });
+
+        const responseUser = {
+            _id: user._id,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            email: user.email,
+            phoneNumber: user.phoneNumber,
+            designation: user.designation,
+        };
+
+        return res.status(200)
+            .cookie("token", token, { 
+                maxAge: 1 * 24 * 60 * 60 * 1000, 
+                httpOnly: true, 
+                sameSite: 'strict' 
+            })
+            .json({
+                message: `Welcome back ${responseUser.firstName} ${responseUser.lastName}`,
+                user: responseUser,
+                success: true
+            });
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message: "An error occurred during login.",
+            success: false
+        });
+    }
+}
+
+
+
+
+export {register, login}
