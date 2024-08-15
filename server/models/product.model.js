1ef90cebe7ed875930ccff1cfe3b9b64bd3e0870
Babloo Kumar <bablookumar24bk@gmail.com>
2024-08-15 22:42:29 +0530

    add product model

---
+++ server/models/product.model.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose'
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    discount: {
+        type: Number,
+        required: true
+    },
+    discounted_price: {
+        type: Number,
+        required: true
+    },
+    prodImage: {
+        type: String,
+        required: true
+    }
+},{timestamps:true});
+
+export const Product = mongoose.model('Product', productSchema);
