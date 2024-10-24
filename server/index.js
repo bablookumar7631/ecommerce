731a8204d9da10ab2d3129556e739c000556bb2f
Babloo Kumar <bablookumar24bk@gmail.com>
2024-10-24 14:27:24 +0530

    "Updated CORS origin to allow both production and local frontend URLs"

--- server/index.js
+++ server/index.js
@@ -42,8 +42,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({extended: true}));
 
 const corsOptions = {
-    origin: 'https://ecommerce-frontend-dmo2.onrender.com',
-    // origin: 'http://localhost:5173',
+    origin: ['https://ecommerce-frontend-dmo2.onrender.com', 'http://localhost:5173'],
     credentials: true,
 };
 app.use(cors(corsOptions));
